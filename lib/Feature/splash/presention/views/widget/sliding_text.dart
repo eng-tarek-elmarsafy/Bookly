2f0fc992afc2adf14eddf8b7eb1d59d981b5724d
tarek_elmarsafy <tarekelmarsafy50@gmail.com>
2025-02-12 21:56:21 +0200

    adding sliding animation to splashView

---
+++ lib/Feature/splash/presention/views/widget/sliding_text.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SlidingText extends StatelessWidget {
+  const SlidingText({
+    super.key,
+    required this.slidingAnimation,
+  });
+
+  final Animation<Offset> slidingAnimation;
+
+  @override
+  Widget build(BuildContext context) {
+    return SlideTransition(
+      position: slidingAnimation,
+      child: const Text(
+        'Read Free Books',
+        style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class BooklyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      theme: ThemeData(
+      theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: const Color(0xff100b30),
       ),
       debugShowCheckedModeBanner: false,
