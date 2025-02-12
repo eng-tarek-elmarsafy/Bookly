e1a44e98a9cb71bcb9e2f0c28ab14099835e6025
tarek_elmarsafy <tarekelmarsafy50@gmail.com>
2025-02-12 18:15:39 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
+import 'Feature/splash/presention/views/splash_view.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      home: SplashView(),
+    );
+  }
+}
+
