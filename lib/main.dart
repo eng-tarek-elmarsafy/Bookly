16fae417bc6ac30385fe7df5c2477a3ff1e57cb9
tarek_elmarsafy <tarekelmarsafy50@gmail.com>
2025-02-12 19:45:36 +0200

    create splashViewbody

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,15 @@ import 'Feature/splash/presention/views/splash_view.dart';
 void main() {
   runApp(const BooklyApp());
 }
+
 class BooklyApp extends StatelessWidget {
   const BooklyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       home: SplashView(),
     );
   }
 }
-
