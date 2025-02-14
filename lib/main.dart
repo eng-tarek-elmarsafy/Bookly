1ae82068a28cee1698b4350c58498bcfad34b97e
tarek_elmarsafy <tarekelmarsafy50@gmail.com>
2025-02-14 02:32:59 +0200

    add googl fonts to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'Feature/splash/presention/views/splash_view.dart';
 
 void main() {
@@ -13,8 +14,9 @@ class BooklyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: const Color(0xff100b30),
-      ),
+          scaffoldBackgroundColor: const Color(0xff100b30),
+          textTheme:
+              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme)),
       debugShowCheckedModeBanner: false,
       home: SplashView(),
     );
