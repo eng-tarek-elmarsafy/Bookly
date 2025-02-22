45746af883b2f0edf94021af2b3e9f249c0476cf
tarek_elmarsafy <tarekelmarsafy50@gmail.com>
2025-02-22 21:35:43 +0200

    fetch Best Seller books

--- lib/Feature/home/data/repos/home_repo_imp.dart
+++ lib/Feature/home/data/repos/home_repo_imp.dart
@@ -1,18 +1,30 @@
 import 'package:bookly_app/Feature/home/data/models/book_model/book.models.dart';
 import 'package:bookly_app/Feature/home/data/repos/home_repo.dart';
 import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:dartz/dartz.dart';
 
-class HomeRepoImp implements HomeRepo{
+class HomeRepoImp implements HomeRepo {
+  ApiService apiService;
+  HomeRepoImp(this.apiService);
+
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() async {
+    try {
+      var data = await apiService.get(endpoint: 'volumes?q=flutter');
+      List<BookModel> books = [];
+
+      for (var book in data['items']) {
+        books.add(BookModel.fromJson(book));
+      }
+      return right(books);
+    } on Exception catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
   Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
     throw UnimplementedError();
   }
 }
--- lib/core/utils/api_service.dart
+++ lib/core/utils/api_service.dart
@@ -3,12 +3,12 @@ import 'package:dio/dio.dart';
 class ApiService {
   final String _baseUrl = 'https://www.googleapis.com/books/v1/';
   final String _apiKey = 'AIzaSyCKph9DHaGVKx4hE3QmyEsfSg-3MFO3CPg';
-  final Dio dio;
+  final Dio _dio;
 
-  ApiService(this.dio);
+  ApiService(this._dio);
 
   Future<Map<String, dynamic>> get({required String endpoint}) async {
-    Response response = await dio.get('$_baseUrl$endpoint&key=$_apiKey');
+    Response response = await _dio.get('$_baseUrl$endpoint&key=$_apiKey');
     return response.data;
   }
 }
