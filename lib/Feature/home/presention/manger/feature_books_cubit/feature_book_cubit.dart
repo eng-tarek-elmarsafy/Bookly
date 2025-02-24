2fa5f7cbeb32dfa3c4a783a5f1471f4771143315
tarek_elmarsafy <tarekelmarsafy50@gmail.com>
2025-02-24 12:23:02 +0200

    create featuer methed

--- lib/Feature/home/presention/manger/feature_books_cubit/feature_book_cubit.dart
+++ lib/Feature/home/presention/manger/feature_books_cubit/feature_book_cubit.dart
@@ -1,11 +1,23 @@
 import 'package:bookly_app/Feature/home/data/models/book_model/book.models.dart';
+import 'package:bookly_app/Feature/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'feature_book_state.dart';
 
 class FeatureBookCubit extends Cubit<FeatureBookState> {
-  FeatureBookCubit() : super(FeatureBookInitial());
+  FeatureBookCubit(this.homeRepo) : super(FeatureBookInitial());
 
-  
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeatureBookLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+
+    result.fold((failure) {
+      emit(FeatureBookFailur(errMessage: failure.err));
+    }, (books) {
+      emit(FeatureBookSuccess(books: books));
+    });
+  }
 }
