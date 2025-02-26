270e3f3d50695cc0cac8be7412f84052aeac3898
tarek_elmarsafy <tarekelmarsafy50@gmail.com>
2025-02-26 12:21:51 +0200

    cached network image

--- lib/Feature/home/presention/views/widget/custom_list_view_item.dart
+++ lib/Feature/home/presention/views/widget/custom_list_view_item.dart
@@ -1,5 +1,5 @@
 import 'package:bookly_app/Feature/home/data/models/book_model/book.models.dart';
-import 'package:bookly_app/core/utils/assets.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class CustomBookImage extends StatelessWidget {
@@ -7,21 +7,48 @@ class CustomBookImage extends StatelessWidget {
   final BookModel? book;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8),
-      child: AspectRatio(
-        aspectRatio: 2.6 / 4,
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            image: DecorationImage(
-              fit: BoxFit.fill,
-              image: NetworkImage(
-                  book?.volumeInfo!.imageLinks!.thumbnail ?? AssetsData.test),
-            ),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(24),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8),
+        child: AspectRatio(
+          aspectRatio: 2.6 / 4,
+          child: CachedNetworkImage(
+            fit: BoxFit.fill,
+            errorWidget: (context, url, error) {
+              return Center(
+                child: Icon(
+                  Icons.error,
+                ),
+              );
+            },
+            progressIndicatorBuilder: (context, url, progress) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+            imageUrl: book?.volumeInfo!.imageLinks!.thumbnail ?? '',
           ),
         ),
       ),
     );
+
+    // Padding(
+    //   padding: const EdgeInsets.symmetric(horizontal: 8),
+    //   child: AspectRatio(
+    //     aspectRatio: 2.6 / 4,
+    //     child: Container(
+    //       decoration: BoxDecoration(
+    //         borderRadius: BorderRadius.circular(16),
+    //         image: DecorationImage(
+    //           fit: BoxFit.fill,
+    //           image: NetworkImage(
+    //             book?.volumeInfo!.imageLinks!.thumbnail ?? AssetsData.test,
+    //           ),
+    //         ),
+    //       ),
+    //     ),
+    //   ),
+    // );
   }
 }
