643add010b079bdbcbdd5a00fe8dddcf22a3a0fc
tarek_elmarsafy <tarekelmarsafy50@gmail.com>
2025-04-08 05:07:30 +0200

    create api service

--- lib/core/utils/api_service.dart
+++ lib/core/utils/api_service.dart
@@ -1,3 +1,12 @@
+import 'package:dio/dio.dart';
+
 class ApiService {
-  
+  final Dio _dio;
+  final baseUrl = 'https://www.googleapis.com/books/v1/';
+  ApiService(this._dio);
+
+  Future<Map<String, dynamic>> get({required String endpoint}) async {
+    Response response = await _dio.get('$baseUrl$endpoint');
+    return response.data;
+  }
 }
