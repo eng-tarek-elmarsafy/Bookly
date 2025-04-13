12dd4f580d269813c459c81fb3455084c6a167f8
tarek_elmarsafy <tarekelmarsafy50@gmail.com>
2025-04-13 10:00:08 +0200

    create server error from dio error

--- lib/Feature/home/data/repos/home_repo_imp.dart
+++ lib/Feature/home/data/repos/home_repo_imp.dart
@@ -4,6 +4,7 @@ import 'package:bookly_app/Feature/home/domain/entities/book_entity.dart';
 import 'package:bookly_app/Feature/home/domain/repos/home_repo.dart';
 import 'package:bookly_app/core/err/failure.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImp extends HomeRepo {
   final HomeRemoteDataSource homeRemoteDataSource;
@@ -14,28 +15,32 @@ class HomeRepoImp extends HomeRepo {
   @override
   Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() async {
     try {
-      List<BookEntity> booksLocal = homeLocalDataSource.fetchFeaturedBooks();
-      if (booksLocal.isNotEmpty) {
-        return right(booksLocal);
+      List<BookEntity> books;
+      books = homeLocalDataSource.fetchFeaturedBooks();
+      if (books.isNotEmpty) {
+        return right(books);
       }
-      List<BookEntity> books = await homeRemoteDataSource.fetchFeaturedBooks();
+      books = await homeRemoteDataSource.fetchFeaturedBooks();
       return right(books);
     } catch (e) {
-      return left(Failure());
+      if (e is DioException) return left(ServerFailure.fromDioException(e));
+      return left(ServerFailure(errMessage: e.toString()));
     }
   }
 
   @override
   Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() async {
     try {
-      List<BookEntity> bookLocal = homeLocalDataSource.fetchNewestBooks();
-      if (bookLocal.isNotEmpty) {
-        return right(bookLocal);
+      List<BookEntity> books;
+      books = homeLocalDataSource.fetchNewestBooks();
+      if (books.isNotEmpty) {
+        return right(books);
       }
-      List<BookEntity> books = await homeRemoteDataSource.fetchNewestBooks();
+      books = await homeRemoteDataSource.fetchNewestBooks();
       return right(books);
     } catch (e) {
-      return left(Failure());
+      if (e is DioException) return left(ServerFailure.fromDioException(e));
+      return left(ServerFailure(errMessage: e.toString()));
     }
   }
 }
--- lib/core/err/failure.dart
+++ lib/core/err/failure.dart
@@ -1,1 +1,58 @@
-class Failure {}
+import 'package:dio/dio.dart';
+
+abstract class Failure {
+  final String errMessage;
+
+  Failure({required this.errMessage});
+}
+
+class ServerFailure extends Failure {
+  ServerFailure({required super.errMessage});
+
+  factory ServerFailure.fromDioException(DioException e) {
+    switch (e.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure(
+            errMessage: 'Connection timeout. Please try again.');
+      case DioExceptionType.sendTimeout:
+        return ServerFailure(
+            errMessage: 'Request timed out while sending data.');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure(
+            errMessage:
+                'Response timed out. The server took too long to respond.');
+      case DioExceptionType.badCertificate:
+        return ServerFailure.fromResponse(e);
+
+      case DioExceptionType.badResponse:
+        return ServerFailure(
+            errMessage: 'Request timed out while sending data.');
+      case DioExceptionType.cancel:
+        return ServerFailure(
+            errMessage: 'Request was cancelled before completion.');
+      case DioExceptionType.connectionError:
+        return ServerFailure(
+            errMessage:
+                'Connection error. Please check your internet connection.');
+      case DioExceptionType.unknown:
+        return ServerFailure(
+            errMessage:
+                'An unexpected error occurred. Please try again later.');
+    }
+  }
+  factory ServerFailure.fromResponse(DioException response) {
+    int statusCode = response.response!.statusCode!;
+    if (statusCode == 404) {
+      return ServerFailure(
+          errMessage: 'Yore response was not found, please try later');
+    } else if (statusCode == 500) {
+      return ServerFailure(
+          errMessage: 'There is a problem with server, please try later');
+    } else if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailure(
+          errMessage: response.response!.data['error']['message']);
+    }
+    return ServerFailure(
+        errMessage: 'An unexpected error occurred. Please try again later.');
+  }
+}
